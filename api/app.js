af1a451c54a7afb6d0a448e67618f443a26d9ef8
Thomas Cherret <thomas.cherret@papernest.com>
2021-08-24 15:06:48 +0200

    setup finish, let start the front

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors');
+const router = require('./routers/router');
+
+const app = express()
+const port = (process.env.PORT || 5000)
+const httpServer = require("http").createServer(app);
+
+app.use(express.json())
+app.use(cors({credentials: true, origin: 'http://localhost:4200' || "https://matcha-heroku.herokuapp.com/"}));
+app.use(router);
+
+// Connecting the server to the port
+
+httpServer.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
