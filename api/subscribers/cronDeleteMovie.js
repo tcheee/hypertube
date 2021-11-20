6bd4c7e7ee8ca040afbdc0a9e54b20320ea62309
Thomas Cherret <thomas.cherret@papernest.com>
2021-11-20 19:36:21 +0100

    add the cron

--- api/subscribers/cronDeleteMovie.js
+++ api/subscribers/cronDeleteMovie.js
@@ -1,14 +1,29 @@
 const cron = require('node-cron');
+const { PrismaClient } = require('@prisma/client');
+const { findSync } = require('@prisma/client/runtime');
+const prisma = new PrismaClient();
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+const fs = require('fs');
 
 function scheduleDeletion() {
-    cron.schedule('* 8 1 * *', () => {
-        console.log('running a task every minute');
+  cron.schedule('* 8 1 * *', async () => {
+    const lastTime = Date.now() - 2592000000;
 
-        //
-        // go and select in the db where the 'last_seen_date' est >= 1 month and put the path in an array
-        // loop the array and delete the movies in the fs.unlin here: https://www.geeksforgeeks.org/node-js-fs-unlink-method/
-        // put the info
-      });
+    const movies = await prisma.movies.findMany({
+      where: {
+        lastTimewatch: {
+          lt: lastTime,
+        },
+      },
+    });
+
+    movies.forEach((movie) => {
+      if (fs.existsSync(appDir + '/tmp/movies/' + movie.hash + '.mp4')) {
+        fs.rmSync(appDir + '/tmp/movies/' + movie.hash + '.mp4');
+      }
+    });
+  });
 }
 
-module.exports = scheduleDeletion
+module.exports = scheduleDeletion;
